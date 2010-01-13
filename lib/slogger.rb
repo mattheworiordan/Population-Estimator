17d5e218647eca13ef73c8373fd7408b7b1e0061
Matthew O'Rirodan <matt@lemonfoundation.com>
2010-01-13 09:44:32 +0000

    Fixed a small bug with the new block support where the startin message was not showing

--- lib/slogger.rb
+++ lib/slogger.rb
@@ -32,7 +32,11 @@ class SLogger
         puts "> #{severity}: #{msg}#{timediff}\n" unless AppConfig.verbose_logging = false
         Time.now
       else
+        RAILS_DEFAULT_LOGGER.send severity, "#{msg}...\n"
+        puts "#{msg}..." unless AppConfig.verbose_logging = false
+        
         timediff = "%0.4fs" % Benchmark.measure { yield }.real
+        
         RAILS_DEFAULT_LOGGER.send severity, "  ... finished #{msg.downcase} in #{timediff}\n"
         puts "  ... finished #{msg.downcase} in #{timediff}\n" unless AppConfig.verbose_logging = false
       end
