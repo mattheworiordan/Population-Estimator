ac2c6e1cc6e22019805836414aa409b17b72e346
Matthew O'Riordan <matthew.oriordan@gmail.com>
2010-05-17 23:55:36 +0100

    Rails 3 updated files - generic
    All of these files could be completely overwritten by the Rails 3 install process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,4 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
-  
-  def set_mime_type_html
-    response.headers['Content-type'] = 'text/html; charset=utf-8'
-  end
-  
-  def render_html_with_flash(args)
-    @flash_results = true
-    set_mime_type_html  # Rails will return text/javscript type by default, and javascript will try and eval this HTML and raise an error
-    render args
-  end
-  
-  # unobtrustive link_to_remote, by default has a # in the href
-  class ActionView::Base
-    def link_to_remote(name, options = {}, html_options = {})  
-      html_options.merge!({:href => url_for(options[:url])}) unless options[:url].blank?  
-      super(name, options, html_options)  
-    end
-  end
+  protect_from_forgery
+  layout 'application'
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_population_estimator_session',
-  :secret      => '78fb978e2a67d55e74dd31ca378313c6fbccaae34f6e10fac6621c819029cdcd128206c665d28b3771ac5148d4bb782d015d11d27c75640d3ec40c4e0fda38c9'
-}
+Rails.application.config.session_store :cookie_store, :key => '_population_estimator_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Rails.application.config.session_store :active_record_store
