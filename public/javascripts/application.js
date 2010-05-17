9e1628d4729e09449e8535acee534c9f499430be
Matthew O'Riordan <matthew.oriordan@gmail.com>
2010-05-18 00:16:38 +0100

    Rails 3 comments prepended to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,6 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
 var map_refs = {};
 
 function ZoomMapTo(map_id, north, south, east, west, latitude_centre, longitude_centre)
